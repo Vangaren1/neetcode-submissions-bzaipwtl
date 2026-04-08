2aabf2cf7758918967f23f27fc3cc19116d28014
Vangaren1 <markthompson@vangaren.com>
2026-04-08 11:10:56 -0400

    Add: palindrome-partitioning - submission-2

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-2.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    vector<vector<string>> partition(string s)
+    {
+        vector<vector<string>> results;
+        vector<string> curr;
+        dfs(s, 0, results, curr);
+        return results;
+    }
+
+
+    void dfs(string &s, int index, vector<vector<string>> &results, vector<string> &curr)
+    {
+        if (index >= s.size())
+        {
+            results.push_back(curr);
+            return;
+        }
+
+        for (int idx = index; idx < s.size(); idx++)
+        {
+            string sub = s.substr(index, idx - index + 1);
+            if (isPalindrome(sub))
+            {
+                curr.push_back(sub);
+                dfs(s, idx + 1, results, curr);
+                curr.pop_back();
+            }
+        }
+    }
+    bool isPalindrome(string s)
+    {
+        int start = 0;
+        int end = s.size()-1;
+        while (start < end)
+        {
+            if (s[start] != s[end])
+            {
+                return false;
+            }
+
+            start++;
+            end--;
+        }
+        return true;
+    }
+};
