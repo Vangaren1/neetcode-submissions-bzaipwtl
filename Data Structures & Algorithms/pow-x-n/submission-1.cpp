17da092fa4492664ac6a8f86610de4f687c382ad
Vangaren1 <markthompson@vangaren.com>
2026-05-27 09:08:06 -0400

    Add: pow-x-n - submission-1

---
+++ Data Structures & Algorithms/pow-x-n/submission-1.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(x==0){
+            return 0;
+        }
+        if(n==0){
+            return 1;
+        }
+        if(n==1){
+            return x;
+        }
+
+        if(n<0){
+            return 1.0 / myPow(x, abs(n));
+        }
+
+        if(n % 2 == 0){
+            double result = myPow(x, n /2);
+            return result * result;
+        }
+
+        return myPow(x, n-1) * x;
+    }
+};
