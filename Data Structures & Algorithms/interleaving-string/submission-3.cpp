5b5140cf9d8d8b80465c3a7327af82b433bee904
Vangaren1 <markthompson@vangaren.com>
2026-06-08 09:12:05 -0400

    Add: interleaving-string - submission-3

---
+++ Data Structures & Algorithms/interleaving-string/submission-3.cpp
@@ -0,0 +1,60 @@
+class Solution
+{
+    struct PairHash
+    {
+        size_t operator()(const pair<int, int> &p) const
+        {
+            size_t h1 = hash<int>{}(p.first);
+            size_t h2 = hash<int>{}(p.second);
+
+            return h1 ^ (h2 << 1);
+        }
+    };
+
+public:
+    bool isInterleave(string s1, string s2, string s3)
+    {
+        if (s1.size() + s2.size() != s3.size())
+        {
+            return false;
+        }
+                if(s1.size()== 0 && s2.size() == 0 && s3.size()==0){
+            return true;
+        }
+        unordered_map<pair<int, int>, bool, PairHash> cache;
+        return rec(0, 0, s1, s2, s3, cache);
+    }
+    bool rec(int index1, int index2, string s1, string s2, string s3, unordered_map<pair<int, int>, bool, PairHash> &c)
+    {
+        if (c.count({index1, index2}))
+        {
+            return c[{index1, index2}];
+        }
+
+        int index3 = index1 + index2;
+
+        if (index3 == s3.size())
+        {
+            return index1 == s1.size() && index2 == s2.size();
+        }
+
+        if (index1 < s1.size() && s1[index1] == s3[index3])
+        {
+            if (rec(index1 + 1, index2, s1, s2, s3, c))
+            {
+                c[{index1, index2}] = true;
+                return true;
+            }
+        }
+        if (index2 < s2.size() && s2[index2] == s3[index3])
+        {
+            if (rec(index1, index2 + 1, s1, s2, s3, c))
+            {
+                c[{index1, index2}] = true;
+                return true;
+            }
+        }
+        c[{index1, index2}] = false;
+        return false;
+    }
+};
