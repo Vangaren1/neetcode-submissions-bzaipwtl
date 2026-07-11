d76370b19f65563322b40b47700930e5170b6e62
Vangaren1 <markthompson@vangaren.com>
2026-07-10 22:35:36 -0400

    Add: baseball-game - submission-0

---
+++ Data Structures & Algorithms/baseball-game/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        stk = []
+        for op in operations:
+            if op not in ("+", "C", "D"):
+                stk.append(int(op))
+            elif op == "+":
+                stk.append(stk[-1] + stk[-2])
+            elif op == "C":
+                stk.pop()
+            elif op == "D":
+                stk.append(2 * stk[-1])
+        return sum(stk)
