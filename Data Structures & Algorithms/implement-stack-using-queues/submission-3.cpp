44b26ca08ebdcda4e7881e099d86ff0be10013ce
Vangaren1 <markthompson@vangaren.com>
2026-07-11 22:38:13 -0400

    Add: implement-stack-using-queues - submission-3

---
+++ Data Structures & Algorithms/implement-stack-using-queues/submission-3.cpp
@@ -0,0 +1,37 @@
+class MyStack
+{
+private:
+    queue<int> q;
+
+public:
+    MyStack()
+    {
+    }
+
+    void push(int x)
+    {
+        q.push(x);
+        for (int i = 0; i < q.size() - 1; i++)
+        {
+            q.push(q.front());
+            q.pop();
+        }
+    }
+
+    int pop()
+    {
+        int tmp = q.front();
+        q.pop();
+        return tmp;
+    }
+
+    int top()
+    {
+        return q.front();
+    }
+
+    bool empty()
+    {
+        return q.empty();
+    }
+};
