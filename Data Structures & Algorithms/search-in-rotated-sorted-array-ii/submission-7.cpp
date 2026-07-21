3a68526accf64498b4a3ce3019cbb173c74fe79c
Vangaren1 <markthompson@vangaren.com>
2026-07-20 20:01:18 -0400

    Add: search-in-rotated-sorted-array-ii - submission-7

---
+++ Data Structures & Algorithms/search-in-rotated-sorted-array-ii/submission-7.cpp
@@ -0,0 +1,49 @@
+class Solution
+{
+public:
+    bool search(vector<int> &nums, int target)
+    {
+        int left(0), mid(0);
+        int right = nums.size() - 1;
+
+        while (left <= right)
+        {
+            mid = left + (right - left) / 2;
+
+            if (nums[mid] == target ||nums[left] == target ||nums[right] == target)
+            {
+                return true;
+            }
+
+            if (nums[mid] == nums[left] && nums[mid] == nums[right])
+            {
+                left++;
+                right--;
+            }
+            else if (nums[left] <= nums[mid])
+            {
+                // sorted left half
+                if (nums[left] < target && target < nums[mid])
+                {
+                    right = mid - 1;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+            else
+            {
+                if (nums[mid] < target && target < nums[right])
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1 ;
+                }
+            }
+        }
+        return false;
+    }
+};
