63b315e75f3c03fbd0810b94fa6750038779df19
Vangaren1 <markthompson@vangaren.com>
2026-08-11 16:16:03 -0400

    Add: course-schedule-iv - submission-8

---
+++ Data Structures & Algorithms/course-schedule-iv/submission-8.py
@@ -0,0 +1,23 @@
+class Solution:
+    def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
+        courses = [set() for _ in range(numCourses)]
+
+        for courseA, courseB in prerequisites:
+            courses[courseA].add(courseB)
+
+        memo = {}
+
+        def dfs(node, target):
+            if (node, target) in memo:
+                return memo[(node, target)]
+            if node == target:
+                memo[(node, target)]=True 
+                return True
+            for neighbor in courses[node]:
+                if dfs(neighbor, target):
+                    memo[(node, target)]=True
+                    return True
+            memo[(node, target)] = False 
+            return False
+
+        return [dfs(node, target) for node, target in queries]
