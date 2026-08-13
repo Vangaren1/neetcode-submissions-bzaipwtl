3bd6c296a24307f816b1b12fed3473ef05fb782b
Vangaren1 <markthompson@vangaren.com>
2026-08-13 10:45:10 -0400

    Add: path-with-minimum-effort - submission-3

---
+++ Data Structures & Algorithms/path-with-minimum-effort/submission-3.cpp
@@ -0,0 +1,50 @@
+class Solution
+{
+public:
+    int minimumEffortPath(vector<vector<int>> &heights)
+    {
+        int height(heights.size()), width(heights[0].size());
+        vector<pair<int, int>> diff = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        vector<vector<int>> dist(height, vector<int>(width, INT32_MAX));
+        dist[0][0] = 0;
+        priority_queue<
+            pair<int, pair<int, int>>,
+            vector<pair<int, pair<int, int>>>,
+            greater<pair<int, pair<int, int>>>>
+            pq;
+        pq.push({0, {0, 0}});
+        while (!pq.empty())
+        {
+            auto [effort, pos] = pq.top();
+            pq.pop();
+            auto [y, x] = pos;
+            if (effort > dist[y][x])
+            {
+                continue;
+            }
+                        if (y == height - 1 && x == width - 1)
+            {
+                return effort;
+            }
+            int curr = heights[y][x];
+            for (auto [dy, dx] : diff)
+            {
+                int ny = y + dy;
+                int nx = x + dx;
+
+                if (0 <= ny && 0 <= nx && ny < height && nx < width)
+                {
+                    int newHeight = heights[ny][nx];
+                    int stepDiff = abs(curr - newHeight);
+                    int newEffort = max(effort, stepDiff);
+                    if (newEffort < dist[ny][nx])
+                    {
+                        dist[ny][nx] = newEffort;
+                        pq.push({newEffort, {ny, nx}});
+                    }
+                }
+            }
+        }
+        return dist[height - 1][width - 1];
+    }
+};
