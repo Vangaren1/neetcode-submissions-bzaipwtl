6629290555ed2ce74c39c4bb04d7b37819eea14d
Vangaren1 <markthompson@vangaren.com>
2026-08-17 15:47:24 -0400

    Add: bitwise-and-of-numbers-range - submission-4

---
+++ Data Structures & Algorithms/bitwise-and-of-numbers-range/submission-4.py
@@ -0,0 +1,16 @@
+class Solution:
+    def rangeBitwiseAnd(self, left: int, right: int) -> int:
+        msbIndexLeft = left.bit_length() - 1
+        msbIndexRight = right.bit_length() - 1
+
+        total = 0
+        while msbIndexLeft >= 0:
+            mask = 1 << msbIndexRight
+            msbIndexRight -= 1
+            msbIndexLeft -= 1
+            lmask = left & mask
+            rmask = right & mask
+            if lmask != rmask:
+                break
+            total |= lmask & mask
+        return total
